364021f9e12e6821d3c54f71f9c6f378837b68ae
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-17 12:44:38 +0200

    updated env

---
+++ api/config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "./../../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
+$dotenv->load();
+
+define("MAIL_FROM",         $_ENV['MAIL_FROM']);
+define("MAIL_APP_PASSWORD", $_ENV['MAIL_APP_PASSWORD']);
+define("MAIL_FROM_NAME",    $_ENV['MAIL_FROM_NAME']);
+?>
