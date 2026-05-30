2e1b3018df60867a5cede99723dc15da92a0a8c6
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-31 01:03:10 +0200

    test

--- api/config/session.php
+++ api/config/session.php
@@ -2,14 +2,32 @@
 
 if (session_status() === PHP_SESSION_NONE) {
     
-    session_set_cookie_params([
-        'lifetime' => 0,            
-        'path' => '/',              
-        'domain' => 'localhost',   
-        'secure' => false,          
-        'httponly' => true,         
-        'samesite' => 'Lax'         
-    ]);
+    $isLocalhost = (
+        isset($_SERVER['HTTP_HOST']) && 
+        (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false)
+    );
+
+    if ($isLocalhost) {
+
+        session_set_cookie_params([
+            'lifetime' => 0,            
+            'path' => '/',              
+            'domain' => 'localhost',   
+            'secure' => false,          
+            'httponly' => true,         
+            'samesite' => 'Lax'         
+        ]);
+    } else {
+ 
+        session_set_cookie_params([
+            'lifetime' => 86400, 
+            'path' => '/',              
+           
+            'secure' => true,    
+            'httponly' => true,  
+            'samesite' => 'None' 
+        ]);
+    }
 
     session_start();
 }
