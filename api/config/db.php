6434708e253116505998f5032e4de3b8150653d8
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-30 19:03:14 +0200

    railway prod

--- api/config/db.php
+++ api/config/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
+// $dotenv->load();
 
 $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
 
